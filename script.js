235a2aa3f2b0db129d32edde3603b65dd6d34bdf
Jade Kingsley <125752320+marshmallowsquish@users.noreply.github.com>
2023-11-07 17:51:16 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* DECLARE VARIABLES - selectors*/
+
+//selectors:
+const gridContainer = document.getElementById("grid-container");
+
+/* DECLARE FUNCTIONS */
+
+/* DECLARE EVENT HANDLERS */
+
+/* DECLARE NAMESPACES */
+
+/* SCRIPT */
