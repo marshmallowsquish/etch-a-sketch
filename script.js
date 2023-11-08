e4e276637ed08a3f64d309a7ff536d6c5fa0c2e5
Jade Kingsley <125752320+marshmallowsquish@users.noreply.github.com>
2023-11-07 20:10:12 -0600

    Add functionality to create new grid button

--- script.js
+++ script.js
@@ -3,17 +3,34 @@
 //selectors:
 const gridContainer = document.getElementById("grid-container");
 const squares = document.getElementsByClassName("square")
+const newGridButton = document.getElementById("new-grid-button")
+const resetButton = document.getElementById("reset-button");
+let gridSize = 16;
 
 /* DECLARE FUNCTIONS */
+function getNewSize() {
+  gridSize = prompt("Enter a number between 4 and 100.", "");
+  gridSize = parseInt(gridSize);
+  if (!(gridSize >= 4 && gridSize <=100)) getNewSize();
+}
 
 /* DECLARE EVENT HANDLERS */
+newGridButton.addEventListener("click", function() {
+  getNewSize();
+  INIT.createGrid();
+  INIT.createMouseoutEvent();
+})
 
 /* DECLARE NAMESPACES */
 const INIT = {
-  createGrid: function(givenNumber) {
-    for (let i = 1; i <= givenNumber**2; i++) {
+  createGrid: function() {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild);
+    }
+    
+    for (let i = 1; i <= gridSize**2; i++) {
       const square = document.createElement("div");
-      const side = (1 / givenNumber) * 100;
+      const side = (1 / gridSize) * 100;
       square.style.minHeight = `${side}%`;
       square.style.minWidth = `${side}%`;
       square.classList.add("square");
@@ -30,5 +47,5 @@ const INIT = {
 }
 
 /* SCRIPT */
-INIT.createGrid(16);
+INIT.createGrid();
 INIT.createMouseoutEvent();
